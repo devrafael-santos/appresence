b7197ef5413ea24307eea7b596e30d1e9eeab983
devrafael-santos <raafael.cs@gmail.com>
2025-03-25 21:18:28 -0300

    Added Student domain

--- back-end/src/main/java/com/raffasdev/backend/controller/StudentController.java
+++ back-end/src/main/java/com/raffasdev/backend/controller/StudentController.java
@@ -1,15 +1,25 @@
 package com.raffasdev.backend.controller;
 
+import com.raffasdev.backend.domain.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController()
 @RequestMapping("/students")
 public class StudentController {
 
     @GetMapping()
-    public String getStudents() { 
-        return "Hello World";
+    public List<Student> getStudents() {
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(UUID.randomUUID(), "John Doe1"));
+        students.add(new Student(UUID.randomUUID(), "John Doe2"));
+
+        return students;
     }
 }
---
+++ back-end/src/main/java/com/raffasdev/backend/domain/Student.java
@@ -0,0 +1,24 @@
+package com.raffasdev.backend.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+}
